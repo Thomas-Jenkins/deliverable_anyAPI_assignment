f99d78288d53de78fa9bde297afbf4e478f8d193
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-11-04 12:46:21 -0700

    wrote passing test for books list

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,31 +1,20 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-const { request } = require('express');
-// const request = require('supertest');
-// const app = require('../lib/app');
-
-// describe('backend-express-template routes', () => {
-//   beforeEach(() => {
-//     return setup(pool);
-//   });
-//   it('example test - delete me!', () => {
-//     expect(1).toEqual(1);
-//   });
-//   afterAll(() => {
-//     pool.end();
-//   });
-// });
+const request = require('supertest');
+const app = require('../lib/app');
+const { books } = require('../lib/books-data');
 
 describe('books routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-});
-
-it('/books should return a list of books', async () => {
-  const res = await request(app).get('/books');
-  const expected = books.map((book) => {
-    return { id: book.id, title: book.title };
+  
+  it('/books should return a list of books', async () => {
+    const res = await request(app).get('/books');
+    const expected = books.map((book) => {
+      return { id: book.id, title: book.title };
+    });
+    expect(res.body).toEqual(expected);
   });
-  expect
-})
+  
+});
