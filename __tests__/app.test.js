788ba5985dfc7a9510fb02c532e9b496b6861f6b
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-11-08 09:31:22 -0800

    second test for books route is passing. Began work on second set of routes and tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,6 +9,19 @@ describe('books routes', () => {
     return setup(pool);
   });
   
+  it('/books/:id should return the details of a single book matching the id', async () => {
+    const res = await request(app).get('/books/1');
+    const theFinalEmpire = {
+      id: '1',
+      title: 'Mistborn: The Final Empire', 
+      author: 'Brandon Sanderson', 
+      year_published: 2006, 
+      first_paragraph: 'Ash fell from the sky. Vin watched the downy flakes drift through the air. Leisurely. Careless. Free. The puffs of soot fell like black snowflakes, descending upon the dark city of Luthadel. They drifted in corners, blowing in the breeze and curling in tiny whirlwinds over the cobblestones. They seemed so uncaring. What would that be like?', 
+      publisher: 'Tor Books'
+    };
+    expect(res.body).toEqual(theFinalEmpire);
+  });
+
   it('/books should return a list of books', async () => {
     const res = await request(app).get('/books');
     const expected = books.map((book) => {
@@ -17,9 +30,15 @@ describe('books routes', () => {
     expect(res.body).toEqual(expected);
   });
   
-  it('/:id should return the details of a single book matching the id', async () => {
-    const res = await request(app).get('/books/1');
-    const 
-  })
-
+  afterAll(() => {
+    pool.end();
+  });
 });
+
+describe('alchemy_assignments routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it
+})
